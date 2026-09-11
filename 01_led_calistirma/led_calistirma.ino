99be69e9fcd248cf9967cacc1d2df06c624894fe
selim-tekeli <f251202021@ktun.edu.tr>
2026-09-12 01:18:41 +0300

    Update led_calistirma.ino

--- 01_led_calistirma/led_calistirma.ino
+++ 01_led_calistirma/led_calistirma.ino
@@ -1,17 +1,9 @@
-const int LED=9;
-
-void setup()
-{
+const int LED = 9; 
+void setup() {
   pinMode(LED, OUTPUT); 
+  digitalWrite(LED, HIGH); 
 }
 
-void loop()
-{
-  for (int i = 100; i <= 1000; i = i + 100) 
-  {
-    digitalWrite(LED, HIGH);
-    delay(i); 
-    digitalWrite(LED, LOW);
-    delay(i);
-  }
+void loop() {
+  
 }
