d6bd3076d24853ddec4819dfe40479e59553a3eb
selim-tekeli <f251202021@ktun.edu.tr>
2026-09-12 12:12:35 +0300

    Update pcden_gelen_verinin_okunmasi_ve_tekrar_pcye_gonderilmesi.ino

--- 11_pcden_gelen_verinin_okunmasi_ve_tekrar_pcye_gonderilmesi/pcden_gelen_verinin_okunmasi_ve_tekrar_pcye_gonderilmesi.ino
+++ 11_pcden_gelen_verinin_okunmasi_ve_tekrar_pcye_gonderilmesi/pcden_gelen_verinin_okunmasi_ve_tekrar_pcye_gonderilmesi.ino
@@ -1,29 +1,13 @@
-const int kirmizi = 11;
-const int yesil = 10;
-const int mavi = 9;
-
-int kDeger = 0;
-int yDeger = 0;
-int mDeger = 0;
-
+char data; 
 void setup() {
-  Serial.begin(9600);
-  pinMode(kirmizi, OUTPUT);
-  pinMode(yesil, OUTPUT);
-  pinMode(mavi, OUTPUT);
+  Serial.begin(9600); 
 }
 
 void loop() {
-  while (Serial.available() > 0) 
+  
+  if (Serial.available() > 0) 
   {
-    kDeger = Serial.parseInt();
-    yDeger = Serial.parseInt();
-    mDeger = Serial.parseInt();
-
-    if (Serial.read() == '\n') {
-      analogWrite(kirmizi, kDeger);
-      analogWrite(yesil, yDeger);
-      analogWrite(mavi, mDeger);
-    }
+    data = Serial.read(); 
+    Serial.print(data); 
   }
 }
