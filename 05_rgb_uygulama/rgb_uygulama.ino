e4c3b7100cc48b215110f6a3249de835bc4dfe96
selim-tekeli <f251202021@ktun.edu.tr>
2026-09-12 11:51:43 +0300

    Update rgb_uygulama.ino

--- 05_rgb_uygulama/rgb_uygulama.ino
+++ 05_rgb_uygulama/rgb_uygulama.ino
@@ -1,84 +1,35 @@
-const int BlueLed = 11;
-const int GreenLed = 10;
-const int RedLed = 9;
-const int buton = 4;
+const int Led=2; 
+const int Buton=4; 
+boolean oncekiDurum=LOW; 
+boolean simdikiDurum=LOW; 
+boolean ledAcikmi=false; 
 
-boolean oncekiBtnDurum = LOW;
-boolean simdikiBtnDurum = LOW;
-
-int ledModu = 0;
-
-void setup() {
-  pinMode(BlueLed, OUTPUT);
-  pinMode(GreenLed, OUTPUT);
-  pinMode(RedLed, OUTPUT);
-  pinMode(buton, INPUT);
+void setup()
+{
+  pinMode(Led, OUTPUT); 
+  pinMode(Buton, INPUT); 
 }
 
-boolean butonKontrol(boolean durum) {
-  boolean simdikiBtn = digitalRead(buton);
-  if (durum != simdikiBtn) {
-    delay(5);
-    simdikiBtn = digitalRead(buton);
-  }
-  return simdikiBtn;
-}
-
-void rgbAyarla(int renk) {
-  
-
-  if (renk == 1) { 
-    digitalWrite(BlueLed, LOW);
-    digitalWrite(GreenLed, LOW);
-    digitalWrite(RedLed, HIGH);
-  }
-  else if (renk == 2) { 
-    digitalWrite(BlueLed, HIGH);
-    digitalWrite(GreenLed, LOW);
-    digitalWrite(RedLed, LOW);
-  }
-  else if (renk == 3) { 
-    digitalWrite(BlueLed, LOW);
-    digitalWrite(GreenLed, HIGH);
-    digitalWrite(RedLed, LOW);
-  }
-  else if (renk == 4) { 
-    analogWrite(BlueLed, 128);
-    analogWrite(GreenLed, 0);
-    analogWrite(RedLed, 128);
-  }
-  else if (renk == 5) { 
-    analogWrite(BlueLed, 128);
-    analogWrite(GreenLed, 128);
-    analogWrite(RedLed, 0);
-  }
-  else if (renk == 6) { 
-    analogWrite(BlueLed, 0);
-    analogWrite(GreenLed, 128);
-    analogWrite(RedLed, 128);
-  }
-  else { 
-    digitalWrite(BlueLed, LOW);
-    digitalWrite(GreenLed, LOW);
-    digitalWrite(RedLed, LOW);
+boolean butonKontrol(boolean sonBtnDgr) 
+{
+  boolean simdiki=digitalRead(Buton); 
+  if(sonBtnDgr!=simdiki) 
+  {
+    delay(5); 
+    simdiki=digitalRead(Buton); 
+    return simdiki; 
   }
 }
 
-void loop() {
-  simdikiBtnDurum = butonKontrol(oncekiBtnDurum);
-
+void loop()
+{
   
-  if (oncekiBtnDurum == LOW && simdikiBtnDurum == HIGH) {
-    ledModu++;
+  simdikiDurum=butonKontrol(oncekiDurum);
+  
+  if(oncekiDurum==LOW && simdikiDurum==HIGH)
+  { 
+    ledAcikmi= !ledAcikmi; 
   }
-
-  oncekiBtnDurum = simdikiBtnDurum;
-
-  
-  if (ledModu == 7) {
-    ledModu = 0;
-  }
-
-  
-  rgbAyarla(ledModu);
+  oncekiDurum=simdikiDurum;
+  digitalWrite(Led, ledAcikmi); 
 }
