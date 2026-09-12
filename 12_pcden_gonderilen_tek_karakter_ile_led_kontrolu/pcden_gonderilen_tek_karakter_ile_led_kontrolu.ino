a5cd09099d8214bb108db52d22b0bea9385ebcbc
selim-tekeli <f251202021@ktun.edu.tr>
2026-09-12 12:20:50 +0300

    Update pcden_gonderilen_tek_karakter_ile_led_kontrolu.ino

--- 12_pcden_gonderilen_tek_karakter_ile_led_kontrolu/pcden_gonderilen_tek_karakter_ile_led_kontrolu.ino
+++ 12_pcden_gonderilen_tek_karakter_ile_led_kontrolu/pcden_gonderilen_tek_karakter_ile_led_kontrolu.ino
@@ -1,29 +1,27 @@
-const int kirmizi = 11;
-const int yesil = 10;
-const int mavi = 9;
+const int led=9; 
+char data ; 
 
-int kDeger = 0;
-int yDeger = 0;
-int mDeger = 0;
-
-void setup() {
-  Serial.begin(9600);
-  pinMode(kirmizi, OUTPUT);
-  pinMode(yesil, OUTPUT);
-  pinMode(mavi, OUTPUT);
+void setup()
+{
+  Serial.begin(9600) ;
+  pinMode(led,OUTPUT) ;
 }
 
-void loop() {
-  while (Serial.available() > 0) 
+void loop()
+{
+  
+  if(Serial.available() >0)
   {
-    kDeger = Serial.parseInt();
-    yDeger = Serial.parseInt();
-    mDeger = Serial.parseInt();
-
-    if (Serial.read() == '\n') {
-      analogWrite(kirmizi, kDeger);
-      analogWrite(yesil, yDeger);
-      analogWrite(mavi, mDeger);
+    data=Serial.read() ; 
+    if(data=='1')
+    {
+      digitalWrite (led,HIGH) ;
+      Serial.println("Led acik") ;
+    }
+    else if (data=='0')
+    {
+      digitalWrite (led,LOW) ;
+      Serial.println("Led Kapali") ;
     }
   }
 }
