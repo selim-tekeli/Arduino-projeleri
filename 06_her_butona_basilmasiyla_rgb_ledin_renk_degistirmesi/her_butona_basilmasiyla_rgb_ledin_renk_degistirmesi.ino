e5d4d8589d4a4763faa48989cd5b543cd8ddeb37
selim-tekeli <f251202021@ktun.edu.tr>
2026-09-12 11:56:47 +0300

    Add RGB LED control with button press

--- 06_her_butona_basilmasiyla_rgb_ledin_renk_degistirmesi/her_butona_basilmasiyla_rgb_ledin_renk_degistirmesi.ino
+++ 06_her_butona_basilmasiyla_rgb_ledin_renk_degistirmesi/her_butona_basilmasiyla_rgb_ledin_renk_degistirmesi.ino
@@ -1,12 +1,86 @@
-const int POT = A0; 
-int deger = 0;     
+const int BlueLed = 11;
+const int GreenLed = 10;
+const int RedLed = 9;
+const int buton = 4;
+
+boolean oncekiBtnDurum = LOW;
+boolean simdikiBtnDurum = LOW;
+
+int ledModu = 0;
 
 void setup() {
-  Serial.begin(9600); 
+  pinMode(BlueLed, OUTPUT);
+  pinMode(GreenLed, OUTPUT);
+  pinMode(RedLed, OUTPUT);
+  pinMode(buton, INPUT);
+}
+
+boolean butonKontrol(boolean durum) {
+  boolean simdikiBtn = digitalRead(buton);
+  if (durum != simdikiBtn) {
+    delay(5);
+    simdikiBtn = digitalRead(buton);
+  }
+  return simdikiBtn;
+}
+
+void rgbAyarla(int renk) {
+  
+  if (renk == 1) {
+    digitalWrite(BlueLed, LOW);
+    digitalWrite(GreenLed, LOW);
+    digitalWrite(RedLed, HIGH);
+  }
+  
+  else if (renk == 2) {
+    digitalWrite(BlueLed, HIGH);
+    digitalWrite(GreenLed, LOW);
+    digitalWrite(RedLed, LOW);
+  }
+  
+  else if (renk == 3) {
+    digitalWrite(BlueLed, LOW);
+    digitalWrite(GreenLed, HIGH);
+    digitalWrite(RedLed, LOW);
+  }
+  
+  else if (renk == 4) {
+    analogWrite(BlueLed, 128);
+    analogWrite(GreenLed, 0);
+    analogWrite(RedLed, 128);
+  }
+  
+  else if (renk == 5) {
+    analogWrite(BlueLed, 128);
+    analogWrite(GreenLed, 128);
+    analogWrite(RedLed, 0);
+  }
+  
+  else if (renk == 6) {
+    analogWrite(BlueLed, 0);
+    analogWrite(GreenLed, 128);
+    analogWrite(RedLed, 128);
+  }
+  
+  else {
+    digitalWrite(BlueLed, LOW);
+    digitalWrite(GreenLed, LOW);
+    digitalWrite(RedLed, LOW);
+  }
 }
 
 void loop() {
-  deger = analogRead(POT); 
-  Serial.println(deger);   
-  delay(500);              
+  simdikiBtnDurum = butonKontrol(oncekiBtnDurum);
+  
+  if (oncekiBtnDurum == LOW && simdikiBtnDurum == HIGH) {
+    ledModu++;
+  }
+  
+  oncekiBtnDurum = simdikiBtnDurum;
+  
+  if (ledModu == 7) {
+    ledModu = 0;
+  }
+  
+  rgbAyarla(ledModu);
 }
